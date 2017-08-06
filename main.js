47686f01dcc0e444ef10947747025e65040f5dcb
dalath <andreas@treudd.com>
2017-08-06 14:41:38 +0200

    I made some language related edits

--- main.js
+++ main.js
@@ -39,12 +39,12 @@ function setupRooms()
 	addRoom(0,0, "The river", "You find yourself in a large cavern. Beyond the sandy beach a river flows to the north. To the south you can see a wide opening in the rock.");
 	addRoom(0,1, "Pear-shaped cave", "The passageway opens into a pear-shaped cave with a rough stone floor, making walking across it somewhat awkward. In one corner of the room is a pile of rubble, mainly stones and dust.");
 	addRoom(0,2, "Small dirty room", "A door in the rock face opens to reveal a small room. The room is dirty and unkempt. A straw mattress lies in one corner. In the centre of the room is a wooden table upon which a candle burns, lighting the room with its flickering flame.", null, "candle");
-	addRoom(1,1, "Gloomy corridor", "You are in a gloomy, stone-flagged corridor, three meters wide, three metres high, with rough stone walls, dripping slightly with dampness and covered in mildew.", "candle");
-	addRoom(2,0, "Worm pipe", "A narrow pipe like corridor which light comes at the other side. It seems you need to crawl to the end.");
+	addRoom(1,1, "Gloomy corridor", "You are in a gloomy, stone-flagged corridor, three meters wide, three meters high, with rough stone walls, dripping slightly with dampness and covered in mildew.", "candle");
+	addRoom(2,0, "Worm pipe", "A narrow pipe like corridor with light coming from the other side. It seems you need to crawl to the end.");
         addRoom(2,1, "Fish fountain", "You enter a small room, bare except for a fountain in the middle. Not a particularly grand affair, the fountain is a small carved fish, and a short jet of water comes from its mouth. A badly scratched wooden sign hangs from the fish: \"--- NOT --- DRIN-\"");
-	addRoom(2,2, "Dirty wall", "You are in a room with paintings but they covered by centuries of dust. You wish you could bring the fountain's water somehow to wash the walls.");
-	addRoom(3,0, "Shiny cup", "At the end of the worm pipe there where a small storage area half a meter wide. It was look like a place to hide something. A hole on top made a way for sunlight to lit a jar-size cup.", null, "Shiny cup");
-	addRoom(3,2, "Big hall" , "After washing dusty walls a giant Inka head apeared. you noticed his left eye is a button. By pressing it you entered this big hall. A bunch of lances at the left and light comes at a hole on the right wall.", "Shiny cup");
+	addRoom(2,2, "Dirty wall", "You are in a room with paintings covered by centuries of dust. You wish you could bring the fountain's water somehow to wash the walls.");
+	addRoom(3,0, "Shiny cup", "At the end of the worm pipe there is a small storage area half a meter wide. It looks like a place to hide something. A hole on top makes way for sunlight to light a jar-sized cup.", null, "shiny cup");
+	addRoom(3,2, "Big hall" , "After washing the dusty walls a giant Inka head appeared. You noticed its left eye is a button. By pressing it you entered this big hall. There are a bunch of lances to the left and light comes from a hole on the right wall.", "shiny cup");
 }
 //
 function enterRoomAt(x_, y_)
